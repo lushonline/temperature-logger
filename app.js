58c7ffc174c391076b611e28a30a9cf79e954ffd
Lushonline <github@lushonline.co.uk>
2022-04-19 10:03:04 +0100

    Include ds18b20 logic

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const { v4: uuidv4 } = require('uuid');
-const asciitable = require('asciitable');
 const consola = require('consola');
+const ds18b20 = require('./lib/ds18b20');
+
 // models
 const db = require('./models');
 
@@ -14,20 +15,24 @@ if (!process.env.INTERVAL) {
   process.exit(1);
 }
 
+// eslint-disable-next-line no-unused-vars
+const dummyGetReadings = () => {
+  return [
+    {
+      sensorId: uuidv4(),
+      value: 99.99,
+      units: 'C',
+    },
+  ];
+};
+
+// eslint-disable-next-line no-unused-vars
+const getReadings = () => {
+  return ds18b20.getAllTemperatureSync();
+};
+
 // ------------------------------------------------------------------------------------
 const main = async () => {
-/*   await db.sequelize
-    .sync()
-    .then(async () => {
-      consola.log('connected to database');
-      await db.Reading.destroy({
-        truncate: true,
-      });
-    })
-    .catch((err) => {
-      consola.error(err);
-    }); */
-
   try {
     await db.sequelize.authenticate();
     consola.log('Connection has been established successfully.');
@@ -41,14 +46,12 @@ const main = async () => {
   const intervalObj = setInterval(async () => {
     counter += 1;
 
-    const result = {
-      sensorId: uuidv4(),
-      value: 99.99,
-      units: 'C',
-    };
+    const result = getReadings();
 
-    await db.Reading.create(result).then((sqlresult) => {
-      consola.log(asciitable([sqlresult.dataValues]));
+    result.map(async (value) => {
+      await db.Reading.create(value, { isNewRecord: true }).catch((err) => {
+        consola.error(err);
+      });
     });
 
     if (counter > 10) {
